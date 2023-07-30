443bea672be95d2c1bcbead8c75b3b7af0e86c9a
AlbaSly <arias.brando.raxel@gmail.com>
2023-07-30 17:23:25 -0600

    Hola 2

--- lib/variables/api.dart
+++ lib/variables/api.dart
@@ -1,2 +1,2 @@
 /**REEMPLAZAS EL HOST (192.168.0.56) POR LA IP LOCAL QUE TE GENERE EL IPV4 */
-const String API_URL = 'http://192.168.0.56:8000';
+const String API_URL = 'https://pase-lista-api.onrender.com';
